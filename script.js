c2b3ed0eadc81a0c53d13b6be433f645eedc9ee4
Michael Sarrazin <michael.sarrazin.ms@gmail.com>
2021-05-17 18:15:19 +0200

    Arret de l'exercice pour ne pas perdre la progression

--- script.js
+++ script.js
@@ -2,3 +2,15 @@
 //     interval: 15000
 //   })
 
+
+
+var container = document.querySelector('.card-body');
+console.log(card-body);
+//je ne peux pas cibler les container. trop vague
+
+window.addEventListener('scroll',()=>{
+    // j'essai de dire que si ma page est en bas et qu'on arrive au max de ma page
+    if (window.scrollY + window.innerHeight >= document.documentElement.scrollHeight){
+        document.createElement()
+        
+    })
